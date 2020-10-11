ad4bc919a5f1ab76ec9b2e1aed6d302d8388f91e
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-11 22:43:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,9 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
   
-  def initialize(name, breed, id=nil)
-    @name = name
-    @breed = breed
+  def initialize(attribute_hash id=nil)
+    @name = attribute_hash[:name]
+    @breed = attribute_hash[:breed]
     @id = id
   end
   
