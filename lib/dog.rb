3b8df8d533d6179091cbb10542d99c3536c8c33d
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-11 22:38:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,13 +87,14 @@ class Dog
       WHERE name = ? AND breed = ?
     SQL
     
-    dog = DB[:conn].execute(sql, name, breed).first
-    if dog 
-      new_dog = self.new_from_db(dog)
+    dog = DB[:conn].execute("SELECT * FROMdogs WHERE name = ? AND breed =?", name, breed).first
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new_from_db(dog)
     else
-      new_dog = self.create({:name => name, :breed => breed})
+      dog = self.create({:name => name, :breed => breed})
     end
-    new_dog
+    dog
   end
   
   def update 
